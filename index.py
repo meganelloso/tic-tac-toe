17e85cf15971e0efaa5c4a1182bd7616c93ef878
meganelloso <54054197+meganelloso@users.noreply.github.com>
2021-08-25 23:45:24 +0800

    add start n move_forward def

--- index.py
+++ index.py
@@ -1,10 +1,3 @@
-###pseudo code###
-#init simple UI
-#print Hi, Player 1. Choose your shot! X or O?
-#if X, print Player 1 goes first! Click a box on where you want to put it.
-#if click, place dun.
-#print Your turn Player 2! Click a box on where you want to put it.
-
 #every after input, check if X has horizontal or vertical X/O.
 
 ###Validations###
@@ -20,7 +13,54 @@
 #list
 #for loop yung |||
 
+#START
+game_position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+again = False
+
+#while again == False:
+
+def start():
+    inpt = False
+    ans = input('Welcome, Player 1! Choose your shot! X or O? ')
+
+    while inpt == False:
+        ans_trim = ans.upper().strip()
+        if ans_trim == 'X' or ans_trim == 'O':
+            return ans
+            inpt == True
+        else:
+            ans = input('Oops, looks like you entered the wrong answer. ' +
+                        'Please choose between X and O. ')
+
+def move_forward(text):
+    if text == 'X':
+        player = 'Player 1'
+    else:
+        player = 'Player 2'
+    
+    inpt = input('f{player} goes first! Pick a position to replace 1-9. ')
+
+    while inpt not in game_position:
+        inpt = input('Pick a position to replace 1-9. ')
+
+
+move_forward(start())
+
+def again(answer):
+    if answer == 'Y':
+
+
+
+
 def init():
+    i, j = 0 
+    while i > 3:
+        while j > 5:
+            modulus = j % 2
+            if modulus == 0:
+                print('|')
+            else:
+                print()
     #print lines and initialize list
     #for loop 3 times
         #for loop 5 times
@@ -35,6 +75,6 @@ def init():
             #print ---------
     #endforloop
 
-init()
+#init()
 
 #func for insert/ask user
