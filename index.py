936c488527ee7a81120ecc970f7ab0e1ea014da6
meganelloso <54054197+meganelloso@users.noreply.github.com>
2021-09-05 16:14:17 +0800

    complete game

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
-game_position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+game_position = [''] * 9
 
 def start():
+    game_position = [''] * 9
+
     inpt = False
     ans = input('Welcome, Player 1! Choose your shot! X or O? ')
 
@@ -13,73 +15,112 @@ def start():
             ans = input('Oops, looks like you entered the wrong answer. ' +
                         'Please choose between X and O. ')
 
-def move_forward(n):
+def display_board():
+    i, j, k = 0, 0, 0
+    while i < 3:
+        if i == 1:
+            j = 3
+        elif i == 2:
+            j = 6
+        
+        print(f' {game_position[j]} | {game_position[j + 1]} | {game_position[j + 2]} ')
+
+        if i < 2:
+            print('--------')
+        
+        i += 1
+
+def player_input(n):
+    finish = False
+
     if n == 'X':
         player = 'Player 1'
     else:
         player = 'Player 2'
     
-    #display tic-tac-toe; display_game()
-    inpt = input(f'{player} goes first! Pick a position to replace 1-9. ')
+    display_board()
+    inpt = int(input(f'{player} goes first! Pick a position to replace 1-9. '))
 
-    while int(inpt) not in game_position:
-        inpt = input(f'Pick a position to replace 1-9. ')
-    
-    continue_game(inpt, player)
+    while inpt > len(game_position) or inpt == 0:
+        inpt = int(input('Invalid number. Pick a position to replace 1-9. '))
 
-def again(answer):
-    if answer == 'Y':
-        start()
-    else:
-        return
+    while finish == False:
+        next_op = place_marker(inpt, n)
+        n = next_op
+        check = win_check()
+        display_board()
 
-#main method
-def continue_game(n, player):
-    #display_updated_game()
-    #with initial append + next player; whats next? //////
-
-    #end
-    inpt = ''
-    while inpt not in ['Y', 'N']:
+        if check == 'C' and next_op != 'T':
+            inpt = int(input(f'Its {next_op}s turn. Pick a position between 1 - 9 to place {next_op} '))
+        elif next_op == 'T':
+            inpt = int(input('Im sorry. The position you picked is already filled. Pick another number. '))
+        elif check == 'F':
+            ask = input('Looks like its a draw. Want to play again? ')
+            finish = True
+        elif check == 'X' or check == 'O':
+            ask = input(f'{check} won! Want to play again? ')
+            finish = True
+        
+    while ask.upper().strip() not in ['Y', 'N']:
         inpt = input('Do you want to play again? [Y/N] ').upper().strip()
 
-    again(inpt)
+    if ask == 'Y':
+        game_position.clear()
+        start()
+    else:
+        return 'T'
+
+def place_marker(inpt, n):
+    indx = inpt - 1
+
+    if(game_position[indx] == ''):
+        game_position[indx] = n
+        
+        if n == 'X':
+            n = 'O'
+        else: n = 'X'
+
+        return n
+    else:
+        return 'T' #try again 
 
 
-#every after input, check if X has horizontal or vertical X/O.
+def win_check():
+    i, j, k, var = 0, 0, 0, 0
 
-###Validations###
-#bawal na lagyan yung may laman
-#notify when theres a winner or tie
-#tie if all full
-#game reset?
+    while i < len(game_position):
+        if game_position[i] != '':
+            var = game_position[i]
+            if i == 0:
+                if (var == game_position[1] and var == game_position[2]) \
+                or (var == game_position[3] and var == game_position[6]) \
+                or (var == game_position[4] and var == game_position[8]):
+                    j = 1
+                    break
+            elif i == 1:
+                if (var == game_position[4] and var == game_position[7]):
+                    j = 1
+                    break
+            elif i == 2:
+                if (var == game_position[5] and var == game_position[8]) \
+                or (var == game_position[4] and var == game_position[6]):
+                    j = 1
+                    break
+            elif i == 3:
+                if (var == game_position[4] and var == game_position[5]):
+                    j = 1
+                    break
+            elif i == 6:
+                if (var == game_position[7] and var == game_position[8]):
+                    j = 1
+                    break
+        else: k += 1
+        i += 1
+    if j == 1:
+        return var
+    elif i == 9 and j == 0 and k > 0:
+        return 'C' #continue game 
+    elif i == 9 and j == 0 and k == 0:
+        return 'F' #full and no one won
 
-###QUESTIONS###
-#how to input if Player 1 is X/O vice versa?
-
-move_forward(start())
-
-def init():
-    i, j = 0 
-    while i > 3:
-        while j > 5:
-            modulus = j % 2
-            if modulus == 0:
-                print('|')
-            else:
-                print()
-    #print lines and initialize list
-    #for loop 3 times
-        #for loop 5 times
-            #if even
-                #print |
-            #else
-                #if 6, 8, 10
-                #lagay list
-                #else blank
-        #endforloop
-        #if loop < 2:
-            #print ---------
-    #endforloop
-
-#func for insert/ask user
+player_input(start())
